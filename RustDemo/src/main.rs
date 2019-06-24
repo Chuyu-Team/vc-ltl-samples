63bc6584b557019a85ea0cd22fdc77d1793174c3
MouriNaruto <Mouri_Naruto@outlook.com>
2019-06-24 23:06:18 +0800

    Add RustDemo project.

---
+++ RustDemo/src/main.rs
@@ -0,0 +1,6 @@
+﻿fn main() {
+    println!("VC-LTL Samples - RustDemo");
+    println!("================================");
+    println!("他喵的 MSVC 2019 工具集坑了我们价值百万美刀的项目！");
+    println!("The F@cking MSVC 2019 toolset ruined our $100 millions project!");
+}
