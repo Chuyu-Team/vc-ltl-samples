cd026e44d429498fb08958715370ac8d6771c2c0
MouriNaruto <Mouri_Naruto@outlook.com>
2019-06-29 01:37:56 +0800

    Implement the VisualStudioConsoleDemo.

--- VisualStudioConsoleDemo/VisualStudioConsoleDemo/VisualStudioConsoleDemo.cpp
+++ VisualStudioConsoleDemo/VisualStudioConsoleDemo/VisualStudioConsoleDemo.cpp
@@ -1,20 +1,19 @@
-﻿// VisualStudioConsoleDemo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <clocale>
+#include <cstdio>
+#include <cwchar>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // I can't use that because of the limitation or bug in VC-LTL.
+    // std::setlocale(LC_ALL, "zh_CN.UTF-8");
+
+    std::setlocale(LC_ALL, "chs");
+
+    std::wprintf(
+        L"VC-LTL Samples - VisualStudioConsoleDemo\n"
+        L"================================\n"
+        L"他喵的 MSVC 2019 工具集坑了我们价值百万美刀的项目！\n"
+        L"The F@cking MSVC 2019 toolset ruined our $100 millions project!\n");
+
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
